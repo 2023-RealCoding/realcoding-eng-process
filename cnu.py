631e01d2d04585c340264795290805dc3613f27e
HyunSoo Kim <39149858+dblepart99@users.noreply.github.com>
2023-03-13 16:40:03 +0900

    Modify cnu.py (#2)
    implement fibonacci, factorial #1

--- cnu.py
+++ cnu.py
@@ -1,6 +1,14 @@
 def fibonacci(n: int) -> int:
-    raise NotImplementedError("Should be implemented")
+    # Base case
+    if n == 0:
+        return 0
+    if n < 2:
+        return 1
+    # Recursive case
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 def factorial(n: int) -> int:
-    raise NotImplementedError("Should be implemented")
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
